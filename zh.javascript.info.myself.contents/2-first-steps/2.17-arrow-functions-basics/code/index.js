46c7cf6ea9bc4555d6ee7d39c8777f9879337308
anngreen <anngreen@qq.com>
2023-01-06 23:21:02 +0800

    箭头函数

---
+++ zh.javascript.info.myself.contents/2-first-steps/2.17-arrow-functions-basics/code/index.js
@@ -0,0 +1,9 @@
+let fun = (param) => param;
+let funName = (param) => {
+  return param;
+};
+console.log(fun('fp'));
+console.log(fun('fp')==='fp');
+console.log('----');
+console.log(funName('fnp'));
+console.log(funName('fnp')==='fnp');
